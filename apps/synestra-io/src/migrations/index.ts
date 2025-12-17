d3cf80dd5945fd9acbde24a6f5898b1e7e0bb38a
Nikolay Satsuk <neograiph@gmail.com>
2025-12-17 22:36:00 +0800

    fix(synestra-io): add pages banner/code migrations

--- apps/synestra-io/src/migrations/index.ts
+++ apps/synestra-io/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251215_083817 from './20251215_083817';
+import * as migration_20251217_224500 from './20251217_224500';
 
 export const migrations = [
   {
     up: migration_20251215_083817.up,
     down: migration_20251215_083817.down,
     name: '20251215_083817'
+  },
+  {
+    up: migration_20251217_224500.up,
+    down: migration_20251217_224500.down,
+    name: '20251217_224500'
   },
 ];
