0ee296d6f2269bf7d406dc0464507b5a974174f9
Nikolay Satsuk <neograiph@gmail.com>
2025-12-22 07:00:12 +0800

    feat: add payload-core baseline migration

---
+++ apps/payload-core/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251221_225940 from './20251221_225940';
+
+export const migrations = [
+  {
+    up: migration_20251221_225940.up,
+    down: migration_20251221_225940.down,
+    name: '20251221_225940'
+  },
+];
