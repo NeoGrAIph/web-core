dc77b4928f6246944af96b88f0502f24517e94ee
Nikolay Satsuk <neograiph@gmail.com>
2025-12-22 17:28:48 +0800

    feat(ui): add nnm new year module and local assets

--- apps/synestra-io/src/app/(frontend)/layout.tsx
+++ apps/synestra-io/src/app/(frontend)/layout.tsx
@@ -12,6 +12,7 @@ import { Providers } from '@/providers'
 import { InitTheme } from '@/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 import { draftMode } from 'next/headers'
+import { NnmNewYear } from '@synestra/ui'
 
 import './globals.css'
 import { getServerSideURL } from '@/utilities/getURL'
@@ -28,6 +29,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
       </head>
       <body>
+        <NnmNewYear />
+        <div className="wrap">
         <Providers>
           <SharePreviewBar />
           <AdminBar
@@ -40,6 +43,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           {children}
           <Footer />
         </Providers>
+        </div>
       </body>
     </html>
   )
