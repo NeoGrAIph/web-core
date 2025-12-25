08887a8eda829457d44b057c9523c41f2f267a55
Nikolay Satsuk <neograiph@gmail.com>
2025-12-25 21:26:41 +0800

    Add initial Payload migration for synestra-io

---
+++ apps/synestra-io/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251225_132621 from './20251225_132621';
+
+export const migrations = [
+  {
+    up: migration_20251225_132621.up,
+    down: migration_20251225_132621.down,
+    name: '20251225_132621'
+  },
+];
