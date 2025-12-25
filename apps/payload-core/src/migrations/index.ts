f62437b04b46bb22d3dc24b75d37565474eed74b
Nikolay Satsuk <neograiph@gmail.com>
2025-12-25 09:46:33 +0800

    feat(payload-core): add AI plugin migration

--- apps/payload-core/src/migrations/index.ts
+++ apps/payload-core/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251221_225940 from './20251221_225940';
+import * as migration_20251225_014610 from './20251225_014610';
 
 export const migrations = [
   {
     up: migration_20251221_225940.up,
     down: migration_20251221_225940.down,
-    name: '20251221_225940'
+    name: '20251221_225940',
+  },
+  {
+    up: migration_20251225_014610.up,
+    down: migration_20251225_014610.down,
+    name: '20251225_014610'
   },
 ];
