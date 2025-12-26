826a22a39cd5782771f97c193049a695698d6186
Nikolay Satsuk <neograiph@gmail.com>
2025-12-26 15:22:41 +0800

    Revert "Add Anthropic AI columns migration"
    This reverts commit e3ea39111cda78a96b3c9a23effd877c585297ab.

--- apps/synestra-io/src/migrations/index.ts
+++ apps/synestra-io/src/migrations/index.ts
@@ -1,6 +1,5 @@
 import * as migration_20251225_132621 from './20251225_132621';
 import * as migration_20251226_065403 from './20251226_065403';
-import * as migration_20251226_071900 from './20251226_071900';
 
 export const migrations = [
   {
@@ -12,10 +11,5 @@ export const migrations = [
     up: migration_20251226_065403.up,
     down: migration_20251226_065403.down,
     name: '20251226_065403'
-  },
-  {
-    up: migration_20251226_071900.up,
-    down: migration_20251226_071900.down,
-    name: '20251226_071900'
   },
 ];
